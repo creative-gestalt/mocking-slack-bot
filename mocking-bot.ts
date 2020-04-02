eed63bd219750d6bb1aa4226385c13db921a5d84
lordweasel <lordweasel6@gmail.com>
2020-04-02 14:29:25 -0600

    cleaned up unnecessary code

--- mocking-bot.ts
+++ mocking-bot.ts
@@ -1,149 +1,112 @@
 import {WebClient} from '@slack/web-api'
-import {RTMClient} from '@slack/'
-import {createServer} from 'http';
 import {BOT_TOKEN, CLIENT_SECRET, SIGNING_SECRET, USER_TOKEN} from "./constants";
 
-// const {createEventAdapter} = require('@slack/events-api');
 const {createMessageAdapter} = require('@slack/interactive-messages');
+const slackInteractions = createMessageAdapter(SIGNING_SECRET);
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+const web = new WebClient(BOT_TOKEN);
 const {users} = require('./models');
 const app = require('express')();
 const http = require('http');
-const bodyParser = require('body-parser');
-
-// const slackEvents = createEventAdapter(SIGNING_SECRET);
-const web = new WebClient(BOT_TOKEN);
-const slackInteractions = createMessageAdapter(SIGNING_SECRET);
-
-const port = process.env.PORT || 3000;
-
-let result = '';
 let response = {};
+let result = '';
 
 app.use('/slack/actions', slackInteractions.expressMiddleware());
 app.post('/slack/commands', bodyParser.urlencoded({extended: false}), slackSlashCommand);
-
-// Start a basic HTTP server
 http.createServer(app).listen(port, () => {
     console.log(`server listening on port ${port}`);
 });
 
-// Slack interactive message handlers
 slackInteractions.action({callbackId: 'send_message'}, (payload, respond) => {
-    console.log(`The user ${payload.user.name} with user_id ${payload.user.id} in team ${payload.team.domain} pressed a button`);
+    console.log(`${payload.user.name} pressed a button`);
 
-    web.users.info({user: payload.user.id}).then((results) => {
-        // @ts-ignore
-        const icon_url = results.user.profile.image_48;
-        // @ts-ignore
-        const username = results.user.profile.display_name_normalized;
-        // respond({
-        //     delete_original: true
-        // });
-        response = buildResponse(icon_url, username);
+    users.findBySlackId(payload.user.id)
+        .then(users => users.sendAlteredMessage(payload.actions[0].value === 'accept')
+            .then(() => {
+                if (users.accepted) {
+
+                    // send as user in a channel
+                    web.users.info({user: payload.user.id}).then((results) => {
+                        // @ts-ignore
+                        const icon_url = results.user.profile.image_48;
+                        // @ts-ignore
+                        const username = results.user.profile.display_name_normalized;
+                        web.chat.postMessage({
+                            channel: payload.channel.id,
+                            text: result,
+                            icon_url: icon_url,
+                            username: username,
+                            response_type: 'in_channel'
+                        }).then(() => {
+                            respond({
+                                delete_original: true
+                            });
+                        }, () => {
+
+                            // if a user posts in a DM, send as bot user.
+                            console.log(payload.user.name + ' posted in a DM');
+                            respond({
+                                // channel: payload.channel.id,
+                                text: 'Unfortunately, slack doesn\'t allow bot interactions without the bot present. Please invite the bot.',
+                                response_type: 'ephemeral',
+                            })
+                        });
+                    }, (error) => {
+                        console.error(error)
+                    });
+                } else {
+                    respond({
+                        delete_original: true
+                    })
+                }
+            })
+        ).catch((error) => {
+        console.error(error);
     });
-    return response;
-
-    // users.findBySlackId(payload.user.id)
-    //     .then(users => users.sendAlteredMessage(payload.actions[0].value === 'accept')
-    //         .then(() => {
-    //             if (users.accepted) {
-    //                 respond({
-    //                     delete_original: true
-    //                 });
-    //                 console.log(payload.channel.id);
-    //                 web.im.list({}).then((results) => { console.log(results.ims) });
-    //                 web.users.info({user: payload.user.id}).then((results) => {
-    //                     // @ts-ignore
-    //                     const icon_url = results.user.profile.image_48;
-    //                     // @ts-ignore
-    //                     const username = results.user.profile.display_name_normalized;
-    //                     try {
-    //                         web.chat.postMessage({
-    //                             channel: payload.channel.id,
-    //                             text: result,
-    //                             // as_user: false,
-    //                             icon_url: icon_url,
-    //                             username: username,
-    //                             response_type: 'in_channel'
-    //                         }).then(() => {
-    //                         }, (error) => {
-    //                             console.log(error)
-    //                         });
-    //                     } catch {
-    //
-    //                     }
-    //                 }, (error) => {
-    //                     console.error(error)
-    //                 });
-    //             } else {
-    //                 respond({
-    //                     delete_original: true
-    //                 })
-    //             }
-    //         })
-    //     ).catch((error) => {
-    //     console.error(error);
-    // });
 });
 
-class MockingBot {
+function getPayload(text) {
+    return {
+        text: text,
+        response_type: 'ephemeral',
+        attachments: [{
+            text: 'Do you want to send this?',
+            callback_id: 'send_message',
+            actions: [
+                {
+                    name: 'send_message',
+                    text: 'Yes',
+                    value: 'accept',
+                    type: 'button',
+                    style: 'primary',
+                },
+                {
+                    name: 'send_message',
+                    text: 'No',
+                    value: 'deny',
+                    type: 'button',
+                    style: 'danger',
+                },
+            ],
+        }],
+    };
+}
 
-    buildResponse(icon, username) {
-        return {
-            // channel: payload.channel.id,
-            text: result,
-            response_type: 'in_channel',
-            icon_url: icon,
-            username: username,
-        }
+function alterMessage(text) {
+    const wordsArray = text.split('');
+    for (let i = 0; i < wordsArray.length; i += 2) {
+        wordsArray[i] = wordsArray[i].toUpperCase();
     }
+    result = wordsArray.toString().replace(/,/g, '');
+    return result;
+}
 
-    getPayload(text) {
-        return {
-            text: text,
-            response_type: 'ephemeral',
-            attachments: [{
-                text: 'Do you want to send this?',
-                callback_id: 'send_message',
-                actions: [
-                    {
-                        name: 'send_message',
-                        text: 'Yes',
-                        value: 'accept',
-                        type: 'button',
-                        style: 'primary',
-                    },
-                    {
-                        name: 'send_message',
-                        text: 'No',
-                        value: 'deny',
-                        type: 'button',
-                        style: 'danger',
-                    },
-                ],
-            }],
-        };
-    }
-
-    function
-
-    alterMessage(text) {
-        const wordsArray = text.split('');
-        for (let i = 0; i < wordsArray.length; i += 2) {
-            wordsArray[i] = wordsArray[i].toUpperCase();
-        }
-        result = wordsArray.toString().replace(/,/g, '');
-        return result;
-    }
-
-    function
-
-    slackSlashCommand(req, res, next) {
-        if (req.body.command === '/mock') {
-            const text = req.body.text;
-            const newText = alterMessage(text);
-            const interactiveButtons = getPayload(newText);
-            res.json(interactiveButtons);
-        }
+function slackSlashCommand(req, res, next) {
+    if (req.body.command === '/mock') {
+        const text = req.body.text;
+        const newText = alterMessage(text);
+        const interactiveButtons = getPayload(newText);
+        res.json(interactiveButtons);
     }
 }
